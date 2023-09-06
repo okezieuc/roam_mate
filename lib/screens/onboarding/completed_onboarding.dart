589976442a80d872fe4290797eeef0de783be055
okezieuc <okezieuc@users.noreply.github.com>
2023-09-06 18:25:54 -0500

    create onboarding completed page
    - this page will be displayed after a user has completed onboarding.
    that is, when the user has set a user name and has chosen a
    display name for their profile.

---
+++ lib/screens/onboarding/completed_onboarding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CompletedOnboarding extends StatelessWidget {
+  const CompletedOnboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Onboarding Completed"),
+    );
+  }
+}
