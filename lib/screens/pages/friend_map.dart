a3bba6466e78b54cc2133c7deadcddc419ebc5b4
okezieuc <okezieuc@users.noreply.github.com>
2023-09-20 22:52:37 -0500

    feat: list a user's friends on map page

--- lib/screens/pages/friend_map.dart
+++ lib/screens/pages/friend_map.dart
@@ -6,6 +6,7 @@ import 'package:roam_mate/utils/controllers/profile_controller.dart';
 import 'package:roam_mate/utils/controllers/user_locations_controller.dart';
 import 'package:roam_mate/utils/determine_position.dart';
 import 'package:roam_mate/utils/show_snackbar.dart';
+import 'package:roam_mate/widgets/user_search/user_profile.dart';
 
 class FriendMap extends StatefulWidget {
   const FriendMap({super.key});
@@ -15,6 +16,9 @@ class FriendMap extends StatefulWidget {
 }
 
 class _FriendMapState extends State<FriendMap> {
+  List<dynamic> currentUsersFriends = [];
+  Map<String, Profile> friendsUserProfiles = {};
+
   void signOut() {
     FirebaseAuth.instance.signOut();
   }
@@ -24,34 +28,53 @@ class _FriendMapState extends State<FriendMap> {
     super.initState();
 
     // fetch a list of the current user's friends from Firestore
-    List<String> currentUsersFriends = [];
+    List<dynamic> currentUsersFriendsData = [];
+    Map<String, Profile> friendsUserProfilesData = {};
+    Map<String, UserLocation> friendsUserLocationsData = {};
+
     friendshipsController
         .doc(FirebaseAuth.instance.currentUser!.uid)
         .get()
         .then(
       (doc) {
-        if (doc.data() != null) {
-          currentUsersFriends = doc.data()!.friends as List<String>;
-        } else {
-          print("error: current user's friends should not be null");
-          return;
-        }
+        currentUsersFriendsData = doc.data()!.friends;
+        return currentUsersFriendsData;
       },
       onError: (e) => print("Error getting document: $e"),
-    );
-
-    // get the profile data for each of the user's friends. store this in a map.
-    Map<String, Profile> friendsUserProfiles = {};
-    profileController
-        .where("userId", whereIn: currentUsersFriends)
-        .get()
-        .then((querySnapshot) {
-      for (var userProfile in querySnapshot.docs) {
-        friendsUserProfiles[userProfile.data().userId] = userProfile.data();
-      }
+    ).then((_) {
+      // get the profile data for each of the user's friends. store this in a map.
+      profileController
+          .where("userId", whereIn: currentUsersFriendsData)
+          .get()
+          .then((querySnapshot) {
+        for (var userProfile in querySnapshot.docs) {
+          friendsUserProfilesData[userProfile.data().userId] =
+              userProfile.data();
+        }
+      });
+    }).then((_) {
+      setState(() {
+        currentUsersFriends = currentUsersFriendsData;
+        friendsUserProfiles = friendsUserProfilesData;
+      });
     });
 
-    // get the location data for each of the user's friends. store this in a map
+    /*.then((_) {
+      print(currentUsersFriends);
+
+      // get the location data for each of the user's friends. store this in a map
+      userLocationsController
+          .where("userId", whereIn: currentUsersFriends)
+          .get()
+          .then((querySnapshot) {
+        for (var userLocation in querySnapshot.docs) {
+          friendsUserLocations[userLocation.data().userId] =
+              userLocation.data();
+        }
+
+        print(friendsUserLocations.toString());
+      });
+    });*/
 
     // update the state
   }
@@ -67,9 +90,10 @@ class _FriendMapState extends State<FriendMap> {
 
                 userLocationsController
                     .doc(FirebaseAuth.instance.currentUser!.uid)
-                    .set(UserLocation(
-                        longitude: location.longitude,
-                        latitude: location.latitude));
+                    .update({
+                  "longitude": location.longitude,
+                  "latitude": location.latitude
+                });
 
                 if (!context.mounted) return;
                 showSnackBar(context, "Updated your location on Firebase");
